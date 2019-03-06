c5fb473c80c4ea470a3b754c80adad95c61e1cb7
jbresner <jbresner@nearestexit.com>
2019-03-06 13:40:33 -0500

    Complete MRR Tour

---
+++ data.js
@@ -0,0 +1,375 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-microform-and-electronic-resources-center",
+      "name": "Microform and Electronic Resources Center",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.919819857665022,
+        "pitch": 0.5422896151196426,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19854611004163125,
+          "pitch": 0.23023559130975357,
+          "rotation": 0,
+          "target": "1-first-floor-hallway-between-main-reading-room-and-merc"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9833426682043012,
+          "pitch": 0.21232132616205313,
+          "title": "Reader Registration",
+          "text": ""
+        },
+        {
+          "yaw": 2.9856265400592292,
+          "pitch": 0.844927872699099,
+          "title": "Reference Desk",
+          "text": ""
+        },
+        {
+          "yaw": 2.7681441058874476,
+          "pitch": 0.2697852557979026,
+          "title": "Copier/Printer",
+          "text": "For detailed information on copying and printing services for Collection Materials at the Library of Congress, please visit https://www.loc.gov/rr/copying.html"
+        },
+        {
+          "yaw": 1.8985634683692494,
+          "pitch": 0.11868821716997502,
+          "title": "Public Workstations",
+          "text": ""
+        },
+        {
+          "yaw": 2.93173123445003,
+          "pitch": 0.057933344225840955,
+          "title": "Microfilm Reference Desk",
+          "text": "For additional information, visit https://www.loc.gov/rr/microform/"
+        },
+        {
+          "yaw": 0.03432511026164775,
+          "pitch": 0.6150853822256455,
+          "title": "Microform and Electronic Resources Center",
+          "text": "Here you can register for a reader card here and access public workstations for research. For additional information about Reader Registration and access to Library of Congress reading rooms, please visit https://www.loc.gov/rr/readerregistration.html"
+        }
+      ]
+    },
+    {
+      "id": "1-first-floor-hallway-between-main-reading-room-and-merc",
+      "name": "First Floor Hallway between Main Reading Room and MERC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.9102612163908592,
+        "pitch": 0.14177443770045883,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.049386077226889,
+          "pitch": -0.10182941742420226,
+          "rotation": 0,
+          "target": "0-microform-and-electronic-resources-center"
+        },
+        {
+          "yaw": 0.13587281263502682,
+          "pitch": 0.2505166977620288,
+          "rotation": 0,
+          "target": "2-reference-assistance-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1074398892023565,
+          "pitch": -0.2897292022935698,
+          "title": "Reader Identification Card Required",
+          "text": "All researchers must sign in here prior to entering either the main reading room or the MERC."
+        }
+      ]
+    },
+    {
+      "id": "2-reference-assistance-room",
+      "name": "Reference Assistance Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.1133782199439644,
+        "pitch": 0.16110731556873148,
+        "fov": 1.5819608047749856
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6827889412472663,
+          "pitch": 0.28479464113169506,
+          "rotation": 0,
+          "target": "1-first-floor-hallway-between-main-reading-room-and-merc"
+        },
+        {
+          "yaw": -2.1240915551383583,
+          "pitch": 0.23749664375265667,
+          "rotation": 25.132741228718363,
+          "target": "3-main-reading-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.1347569476095263,
+          "pitch": 0.3283265846396297,
+          "title": "Reference Desk",
+          "text": ""
+        },
+        {
+          "yaw": 1.2693617381308613,
+          "pitch": 1.3322731978217206,
+          "title": "Reference Desk",
+          "text": ""
+        },
+        {
+          "yaw": 3.1134319704121367,
+          "pitch": -0.028457982548429683,
+          "title": "Reference Desk Collection",
+          "text": "<div>This ready-reference collection of approximately 800 titles is located in the Reference Assistance Room (RAR) at the entrance to the reading room (LJ 100).&nbsp; &nbsp;Here reference librarians are on duty during the hours the Main Reading Room is open. The Reference Desk titles are the volumes most frequently consulted by researchers and librarians. The titles include almanacs, dictionaries, encyclopedias, indexes, statistical sources, directories, guides to colleges and scholarships, thesauri, poetry indexes, guides to government publications, and directories of periodicals.</div><div><br></div><div>The reference desk collection for Local History and Genealogy Reference Services is also located in the Reference Assistance Room on the right side as you enter the room.</div><div><br></div><div>The microform guides which have Library of Congress call numbers are also located in the RAR on the right. For additional information please visit http://www.loc.gov/rr/main/collect.html</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-main-reading-room",
+      "name": "Main Reading Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.012160795213237,
+        "pitch": 0.1597662560505917,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.426030823759504,
+          "pitch": 0.1177541190548741,
+          "rotation": 0,
+          "target": "4-alcove-4"
+        },
+        {
+          "yaw": -3.061985436066827,
+          "pitch": 0.15957131246552692,
+          "rotation": 0,
+          "target": "2-reference-assistance-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.4014687908411387,
+          "pitch": 0.43005044257838065,
+          "title": "Main Reading Room Reference Collections",
+          "text": "<div>The Main Reading Room (MRR) houses a large general reference collection emphasizing humanities, social sciences, and bibliography. The mission of the Main Reading Room Reference Collection is to provide comprehensive reference and research materials in the humanities, especially religion, philosophy, language, literature, art, and history, as well as in biography, bibliography, and library science. The collection also covers the social sciences, especially anthropology, criminology, education, political science, psychology, sociology, and women's studies. (for business and economics see Business Reference Services), especially psychology, sociology, political science, and education. Only a few titles are held in subjects where a specialized reading room exists such as business, music, law, and science and technology.</div><div><br></div><div>The MRR collection contains over 16,000 titles in about 56,000 volumes. The books are on open shelves in the alcoves (MRR Alc) of the Main Reading Room on both the main floor and the first balcony; on Decks 7, 46, and 16 adjacent to the reading room; and in the Reference Assistance Room (MRR Ref Desk) as you enter the reading room. Subject classes A-P are shelved in the reading room; classes Q-Z are shelved on Deck 7.</div><div><br></div><div>The titles assigned to the MRR reference collection are designated Reference – Main Reading Room in the online public access catalog. All reference copies appear last--at the bottom of the list of copies in the OPAC. To find out what is available on the open shelves of the Main Reading Room for a particular subject or classification number, do a keyword search including MRR (e.g., MRR adult education, MRR Biog Australia, MRR JK1012, MRR Ref Desk Z733.U6). Reference books are instantly available; they do not have to be ordered from the stacks. For additional information please visit http://www.loc.gov/rr/main/collect.html&nbsp;</div><div><br></div>"
+        },
+        {
+          "yaw": 2.4958502934958284,
+          "pitch": 0.5952073629706263,
+          "title": "Main Reading Room Central Desk",
+          "text": "This is where researchers may pick up books they requested."
+        },
+        {
+          "yaw": -1.3962452188408996,
+          "pitch": 0.1173985562995199,
+          "title": "Copiers/Printers and Copy Cards",
+          "text": "Located in Alcove 7"
+        },
+        {
+          "yaw": 1.9138018960165928,
+          "pitch": 0.13569640766882962,
+          "title": "Scanner",
+          "text": "Located in front of Alcove 3"
+        },
+        {
+          "yaw": -1.8973738528931214,
+          "pitch": 0.3084274533994922,
+          "title": "Desks for Reseachers",
+          "text": ""
+        },
+        {
+          "yaw": -2.3021048410074147,
+          "pitch": 0.14771782888287888,
+          "title": "Card catalog A-P",
+          "text": "Alcove 6/Deck 33"
+        },
+        {
+          "yaw": -1.5241152292114926,
+          "pitch": 0.17813060085925692,
+          "title": "Card Catalog P-Z",
+          "text": "near Alcove 7/Deck 16"
+        }
+      ]
+    },
+    {
+      "id": "4-alcove-4",
+      "name": "Alcove 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.19924794913474386,
+        "pitch": -0.08469790482612538,
+        "fov": 1.5674489150407742
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0179211445666816,
+          "pitch": 0.041109282747530784,
+          "rotation": 0,
+          "target": "3-main-reading-room"
+        },
+        {
+          "yaw": -0.07077360571073754,
+          "pitch": -0.586815359017308,
+          "rotation": 0.7853981633974483,
+          "target": "5-alcove4-deck"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-alcove4-deck",
+      "name": "Alcove4-Deck",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.4156915358127211,
+        "pitch": 0.16755160819144166,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20995218971328278,
+          "pitch": 0.09055474441514377,
+          "rotation": 0,
+          "target": "3-main-reading-room"
+        },
+        {
+          "yaw": 0.244827736809329,
+          "pitch": 0.7533037344714657,
+          "rotation": 8.63937979737193,
+          "target": "4-alcove-4"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Library of Congress Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
